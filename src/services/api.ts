bfe2b82c4b4db20530c8002b18872297ef87628e
mortal pjero <mortalpjero@gmail.com>
2023-11-21 13:41:57 +0000

    feat: initialized project

---
+++ src/services/api.ts
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+const API_URL = 'http://localhost:300/todos';
+
+type Task = {
+  id: number,
+  title: string,
+  description: string,
+  completed: boolean,
+}
+
+const getTasks = () => {
+  return axios.get(API_URL)
+  .then((response) => response.data);
+}
+
+const createTask = (task: Task) => {
+  return axios.post(API_URL, task)
+  .then((response) => response.data);
+};
+
+const updateTask = (task: Task) => {
+  return axios.put(`${API_URL}/${task.id}`, task)
+  .then((response) => response.data);
+};
+
+const deleteTask = (task: Task) => {
+  return axios.delete(`${API_URL}/${task.id}`)
+  .then((response) => response.data);
+}
+
+export {createTask, updateTask, deleteTask, getTasks};
