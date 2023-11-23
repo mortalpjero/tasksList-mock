647f44f813e98f93fb2c8af3e05cea6346a70de2
mortal pjero <mortalpjero@gmail.com>
2023-11-23 20:02:04 +0000

    feat: modified the way task is sent to server

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,31 +2,34 @@ import axios from "axios";
 
 const API_URL = 'http://localhost:3000/todos';
 
-type Task = {
-  id: number,
+type NewTask = {
   title: string,
   description: string,
   completed: boolean,
 }
 
+type ExistingTask = NewTask & {
+  id: string,
+}
+
 const getTasks = () => {
   return axios.get(API_URL)
-  .then((response) => response.data);
+    .then((response) => response.data);
 }
 
-const createTask = (task: Task) => {
+const createTask = (task: NewTask) => {
   return axios.post(API_URL, task)
-  .then((response) => response.data);
+    .then((response) => response.data);
 };
 
-const updateTask = (task: Task) => {
+const updateTask = (task: ExistingTask) => {
   return axios.put(`${API_URL}/${task.id}`, task)
-  .then((response) => response.data);
+    .then((response) => response.data);
 };
 
-const deleteTask = (task: Task) => {
+const deleteTask = (task: ExistingTask) => {
   return axios.delete(`${API_URL}/${task.id}`)
-  .then((response) => response.data);
+    .then((response) => response.data);
 }
 
-export {createTask, updateTask, deleteTask, getTasks};
+export { createTask, updateTask, deleteTask, getTasks };
