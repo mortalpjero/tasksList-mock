b43469c399579ee22b8c82086d70b657cb613a93
mortal pjero <mortalpjero@gmail.com>
2023-11-23 20:04:19 +0000

    style: changed the UI of footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="px-5 md:px-20 lg:px-48 mt-auto">
+    <footer className="px-5 mt-6 md:px-20 lg:px-48 mt-auto">
       <div className="bg-gray-100 rounded-lg shadow dark:bg-gray-300">
         <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
           <span className="text-teal-500 sm:text-center dark:text-gray-400">© 2023 Tasks Manager.
