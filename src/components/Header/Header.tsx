5e94f918a1f6d2065efa65b2ae8dbffd8d91f823
mortal pjero <mortalpjero@gmail.com>
2023-11-25 17:25:50 +0000

    style: changed Header UI

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,8 +2,8 @@ import React from "react"
 
 const Header: React.FC = () => {
   return(
-    <header className="px-5 md:px-20 lg:px-48">
-      <div className="bg-gray-200 rounded h-16 shadow px-5 flex">
+    <header>
+      <div className="bg-gray-200 rounded h-16 shadow flex px-5 md:px-20 lg:px-72">
         <p className="text-3xl block my-auto font-bold">Tasks List</p>
       </div>
     </header>
