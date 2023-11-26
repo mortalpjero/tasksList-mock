30d7fb91082ca7458a77b45b1aba1801373bba91
mortal pjero <mortalpjero@gmail.com>
2023-11-26 10:24:19 +0000

    feat: added fake server functionality for Vercel

---
+++ server.ts
@@ -0,0 +1,18 @@
+import path from 'path';
+import express from 'express';
+import jsonServer from 'json-server';
+
+const app = express();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.use('/db', middlewares, router);
+
+const port = process.env.PORT || 3000;
+app.listen(port);
